95254742ddf340a28115007a2f73f4407bcd23ec
KaminduN <kamindu.nanayakkara@gmail.com>
2018-07-10 23:12:09 +0530

    store app restructure

---
+++ product-app/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  title: 'shop-online.demo',
+  apiUrl: 'http://store-api.example.com/api',
+  contentApiUrl: 'http://store-api.example.com',
+  productApi: '/products',
+  contentApi: '/files/',
+  defaultimageUrl:'assets/images/testProduct.png',
+  placeholdImage: 'assets/images/default.png'
+};
